e67925eb44302c1c86d36c640878e4668928dae2
Emma Slight <emma@thenextchapter.studio>
2022-03-22 19:09:41 +1300

    fix: styles import in _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import Layout from '../components/layout'
 
-import '../styles/globals.css'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
